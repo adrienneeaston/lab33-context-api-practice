a5852a7ddfc6c291fbb3c4d4ce7cb47578d6a65c
adrienneeaston <39181121+adrienneeaston@users.noreply.github.com>
2019-09-25 20:01:24 -0700

    start of refactor

--- src/components/counter/counter.js
+++ src/components/counter/counter.js
@@ -1,36 +1,36 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
+import Increment from "../increment/increment";
 
-const Count = () => {
-
-  const [count, setCount] = useState(0);
+const Count = props => {
+  // const [count, setCount] = useState(0);
 
   const styles = {
     container: {
-      alignItems: 'center',
-      display: 'flex',
-      flexDirection: 'row',
-      justifyContent: 'space-evenly',
-      width: '40%',
+      alignItems: "center",
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "space-evenly",
+      width: "40%"
     },
     counter: {
-      display: 'inline-block',
-      fontSize: '1em',
-      border: '2px solid #444',
-      borderRadius: '50%',
-      lineHeight: '5em',
-      textAlign: 'center',
-      width: '5em',
+      display: "inline-block",
+      fontSize: "1em",
+      border: "2px solid #444",
+      borderRadius: "50%",
+      lineHeight: "5em",
+      textAlign: "center",
+      width: "5em"
     },
     button: {
-      height: '3em',
-    },
+      height: "3em"
+    }
   };
 
   return (
     <div style={styles.container}>
-      <button style={styles.button} onClick={() => setCount(count-1)}> - </button>
-      <span style={styles.counter}>{count}</span>
-      <button style={styles.button} onClick={() => setCount(count+1)}> + </button>
+      {/* <Increment/> */}
+
+      <span style={styles.counter}>{props.babana.counter}</span>
     </div>
   );
 };
